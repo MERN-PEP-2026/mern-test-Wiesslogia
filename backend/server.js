b19e55de325b4e5259cccdf2591bbb5f92e54822
Wiesslogia <gautamprajapati799@gmail.com>
2026-02-27 09:39:33 +0530

    server created

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+// Sample route
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
